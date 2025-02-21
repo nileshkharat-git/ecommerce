fa51891c5d17a4347926217f5fe707fc8e32fea7
nilesh <nileshk262000@gmail.com>
2025-02-21 22:30:20 +0530

    Shop page for product listing purpose

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CardList } from '../components'
+
+const Shop = () => {
+
+  return (
+    <main className='w-10/12 mx-auto'>
+      <CardList />
+    </main>
+  )
+}
+
+export default Shop
