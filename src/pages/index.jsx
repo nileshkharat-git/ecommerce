7f8bbec22e77dae5631f19b50010cc7b6fbc47cd
nilesh <nileshk262000@gmail.com>
2025-02-21 22:31:29 +0530

    Common file to access pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "./Layout";
+import About from "./About";
+import AddProduct from "./AddProduct";
+import Shop from "./Shop";
+import Login from "./Login";
+import Signin from "./Signin";
+import Cart from "./Cart";
+
+export { Layout, About, AddProduct, Shop, Login, Cart, Signin }
